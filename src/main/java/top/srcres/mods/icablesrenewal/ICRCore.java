d21752e3a8d94c727dea4235dbeca7ec87183a26
src_resources <src.res.211@gmail.com>
2024-01-29 20:35:43 +0800

    Add SPDX license headers
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- src/main/java/top/srcres/mods/icablesrenewal/ICRCore.java
+++ src/main/java/top/srcres/mods/icablesrenewal/ICRCore.java
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
 package top.srcres.mods.icablesrenewal;
 
 import top.srcres.mods.icablesrenewal.modules.ICRModuleManager;
--- src/main/java/top/srcres/mods/icablesrenewal/util/FMLHelper.java
+++ src/main/java/top/srcres/mods/icablesrenewal/util/FMLHelper.java
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
 package top.srcres.mods.icablesrenewal.util;
 
 import net.minecraftforge.fml.ModList;
